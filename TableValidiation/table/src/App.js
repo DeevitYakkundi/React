169401511a6b3452e5cc2e55b854b86f44ea40f4
DeevitYakkundi <deevit0601@gmail.com>
2024-04-24 09:51:16 +0530

    Added Problem Statement

---
+++ TableValidiation/table/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TableComponent from './components/TableComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Table Validation</h1>
+      <TableComponent/>
+    </div>
+  );
+}
+
+export default App;
